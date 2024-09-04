331f44916a69488e55f61811d077f57432277b45
Dominic Canora <dominiccanora@gmail.com>
2024-09-04 18:39:01 -0400

    Multithreading? Hopefully?

--- app/utils/all.py
+++ app/utils/all.py
@@ -1,12 +1,39 @@
+import concurrent.futures
+
 from app.utils import graphy, greedyroom, sum, swap
 
 
 def do_all():
     solns = []
 
-    # solns.extend(graphy.generate_solutions())
+    solns.extend(graphy.generate_solutions())
     solns.extend(greedyroom.generate_solutions(10000))
-    solns.extend(sum.generate_solutions(9000))
+    solns.extend(sum.generate_solutions(10000))
 
     for sid in solns:
         swap.tune_solution_by_id(sid, 10)
+
+
+def run_in_parallel():
+    tune_future_list = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+        futures = [
+            executor.submit(graphy.generate_solutions),
+            executor.submit(greedyroom.generate_solutions, 10000),
+            executor.submit(sum.generate_solutions, 10000),
+        ]
+
+        for future in concurrent.futures.as_completed(futures):
+            results = future.result()
+
+            for result in results:
+                tune_future_list.append(
+                    executor.submit(swap.tune_solution_by_id, result, 10)
+                )
+
+        final_results = []
+        for future in concurrent.futures.as_completed(tune_future_list):
+            final_results.append(future.result())
+
+    return final_results
