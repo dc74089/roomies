8f1b21238242343607b4ad2ef19f06c12b6f7296
Dominic Canora <dominiccanora@gmail.com>
2025-02-25 20:55:15 -0500

    Order by name in admin

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from app.models import *
 
+class NameSortAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
 # Register your models here.
-admin.site.register(Person)
+admin.site.register(Person, NameSortAdmin)
 admin.site.register(Request)
 admin.site.register(Solution)
 admin.site.register(SiteConfig)
